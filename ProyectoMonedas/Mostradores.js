2a15a7e08eac7c9ac5cae65e7e9be46639410753
Carlos757 <carlos123df@gmail.com>
2018-12-09 19:01:30 -0700

    Proyecto

--- ProyectoMonedas/Mostradores.js
+++ ProyectoMonedas/Mostradores.js
@@ -1,73 +1,53 @@
-var indice=localStorage.getItem("indice")
-	
+var indice = localStorage.getItem("indice");
+var grupo = localStorage.getItem("mostrador");
+const { BrowserWindow } = require("electron").remote;
+const app = require("electron").app;
+const path = require("path");
+const url = require("url");
+let PantallaMonedas;
 
-	const {BrowserWindow}=require('electron').remote
-	const app=require('electron').app
-	const path=require('path')
-	const url=require('url')
-	let PantallaMonedas;
+var btnMostradores = document.getElementsByClassName("btnMostrar");
 
-	var btnMostradores=document.getElementsByClassName('btnMostrar');
+var VentanaMonedas = function() {
+  localStorage.setItem("mostrador", this.value);
 
+  PantallaMonedas = new BrowserWindow({ width: 620, height: 600 });
+  PantallaMonedas.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "VentanaMonedas.html"),
+      protocol: "file",
+      slashes: true
+    })
+  );
 
-	var VentanaMonedas = function(){
+  PantallaMonedas.show();
+};
 
-		
-		localStorage.setItem("mostrador",this.value)
+var buscaMostradores = function() {
+  var url = "http://museobillete.azurewebsites.net/api/Expo/";
+  console.log(url + indice);
+  fetch(url + indice)
+    .then(datos => datos.json())
+    .then(datos => {
+      mostradores = datos.mostradores;
+      var resultado = "";
+      for (let i = 0; i < mostradores.length; i++) {
+        resultado += `
 
-		PantallaMonedas=new BrowserWindow({width:700,height:600});
-		PantallaMonedas.loadURL(url.format({
+		<article id="ventanaMost">
+			<span id="most">${datos.mostradores[i].titulo}</span>
+          	<img src="img/flecha.png" alt="">
+          	<button class="btnMostrar btnMostradores" id="btnVer"value="${i}">Ver</button>        
+		</article>
+			`;
+      }
+      resultado += "</ol>";
+      document.getElementById("Mostradores").innerHTML = resultado;
 
-			pathname: path.join(__dirname,'VentanaMonedas.html'),
-			protocol: 'file',
-			slashes: true
-		}))
+      for (let i = 0; i < btnMostradores.length; i++) {
+        btnMostradores[i].addEventListener("click", VentanaMonedas);
+      }
+    });
+};
 
-		PantallaMonedas.show();
-
-
-
-	}
-
-
-
-
-var buscaDetalleComics = function(){
-
-	var url="http://museobillete.azurewebsites.net/api/Expo/";
-	console.log(url+indice);
-	fetch(url+indice)
-	.then(datos=>datos.json())
-	.then(datos=>{
-
-		mostradores=datos.mostradores;
-		var resultado="";
-		for(let i=0;i<mostradores.length;i++){
-			resultado += `
-				<br>
-				Mostrador ${i+1}
-				<span class="linea"> ------------------------------ </span>
-				<button class="btnMostrar btnMostradores" value="${i}">Mostrar</button> 
-				<br><br>
-				<hr>
-
-			`
-
-		}
-		resultado +="</ol>"
-		document.getElementById('Mostradores').innerHTML = resultado
-
-		for(let i=0;i<btnMostradores.length;i++){
-
-				btnMostradores[i].addEventListener('click',VentanaMonedas);
-
-
-		 }
-
-	})
-
-
-}
-
-
-buscaDetalleComics();
+buscaMostradores();
--- ProyectoMonedas/index.js
+++ ProyectoMonedas/index.js
@@ -1,89 +1,83 @@
-window.onload= function(){
+window.onload = function() {
+  const { BrowserWindow } = require("electron").remote;
+  const app = require("electron").app;
+  const path = require("path");
+  const url = require("url");
+  let PantallaMostradores;
+  let PantallaFavoritos;
+  var btnMostradores = document.getElementsByClassName("btnMostradores");
 
-	const {BrowserWindow}=require('electron').remote
-	const app=require('electron').app
-	const path=require('path')
-	const url=require('url')
-	let PantallaMostradores;
+  var Mostradores = function() {
+    //alert(this.value);
+    var variable = "";
 
-	var btnMostradores=document.getElementsByClassName('btnMostradores');
-	
-	var Mostradores = function(){
-		//alert(this.value);
-		var variable="";
+    var urls = "http://museobillete.azurewebsites.net/api/Expo";
+    fetch(urls)
+      .then(datos => datos.json())
+      .then(datos => {
+        variable = datos[this.value].id;
+        console.log(variable);
+        localStorage.setItem("indice", variable);
+      });
 
-		var urls="http://museobillete.azurewebsites.net/api/Expo";
-		fetch(urls)
-		.then(datos=>datos.json())
-		.then(datos=>{
-			variable=datos[this.value].id;
-			console.log(variable);
-			localStorage.setItem("indice",variable)
-		})
-		
-		
-		PantallaMostradores=new BrowserWindow({width:400,height:540});
-		PantallaMostradores.loadURL(url.format({
+    PantallaMostradores = new BrowserWindow({ width: 400, height: 540 });
+    PantallaMostradores.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "Mostradores.html"),
+        protocol: "file",
+        slashes: true
+      })
+    );
 
-			pathname: path.join(__dirname,'Mostradores.html'),
-			protocol: 'file',
-			slashes: true
-		}))
+    PantallaMostradores.show();
+  };
 
-		PantallaMostradores.show();
-	}
+  var MostrarVitrinas = function() {
+    // personaje=document.getElementById('txtPersonaje').value;
+    var url = "http://museobillete.azurewebsites.net/api/Expo";
+    fetch(url)
+      .then(datos => datos.json())
+      .then(datos => {
+        var vitrina = "";
+        var foto = "";
+        document.getElementById("abajo").innerHTML = "";
+        for (let i = 0; i < datos.length; i++) {
+          vitrina = datos[i].titulo;
+          foto = datos[i].imagenFondoUrl;
 
-	var MostrarVitrinas=function(){
+          document.getElementById("abajo").innerHTML += `
+          <article class="abajoIzquierda contenido info">
+				<img src="${foto}" class="imgFoto">
+				<span class="txtNombre roboto">${vitrina}</span>
+				<button class="btnMostradores roboto" id="btnMos" value="${i}">Mostrador</button>
 
-		// personaje=document.getElementById('txtPersonaje').value;
-		var url="http://museobillete.azurewebsites.net/api/Expo";
-		fetch(url)
-		.then(datos=>datos.json())
-		.then(datos=>{
+		   </article>
+			
 
-			var vitrina="";
-			var foto=''
-			document.getElementById('abajo').innerHTML='';
-			for(let i=0;i<datos.length;i++){
-				vitrina=datos[i].titulo;
-				foto=datos[i].imagenFondoUrl;
-				
-				
- 
-			document.getElementById('abajo').innerHTML+=`
-				<article class="abajoIzquierda contenido">
-					<img src="${foto}" class="imgFoto">
-				</article>
-				<article class="abajoDerecha">
-					<div class="txtNombre roboto">${vitrina}</div>
-					<button class="btnMostradores roboto" value="${i}">Mostrador</button> 
-				</article>
-				<hr>
-				<br> 
+			`;
+        } //termina for
+        for (let i = 0; i < btnMostradores.length; i++) {
+          btnMostradores[i].addEventListener("click", Mostradores);
+        }
+      });
+  };
 
-			`
-
-			} //termina for
-			for(let i=0;i<btnMostradores.length;i++){
-
-				btnMostradores[i].addEventListener('click',Mostradores);
-
-
-			 }
-		})
-	}
-
-
-	MostrarVitrinas();
-
-
-	// var btnBuscar=document.getElementById('btnBuscar');
-	// btnBuscar.addEventListener('click',buscaPersonaje);
+  var muestraFavoritos=function(){                
+    localStorage.setItem("fav",this.value);
+    localStorage.setItem("favoritos",favoritos);
+    pantallaDetalle=new BrowserWindow({width:610,height:500})
+    pantallaDetalle.loadURL(url.format({
+      pathname: path.join(__dirname,'PantallaFavoritos.html'),
+      protocol:'file',
+      slashes:true
+    }))    
+  }
 
 
 
 
-}
-
-
-
+  MostrarVitrinas();
+  var btnFavoritos=document.getElementById('btnFavoritos');
+  btnFavoritos.addEventListener('click',muestraFavoritos)
+  
+};
